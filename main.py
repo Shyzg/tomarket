aac917b7168e2dc1a1366db2f1480a70c90065c7
Al Jarreau Arman Maulana <shyzago@gmail.com>
2024-07-31 16:10:53 +0700

    Aw

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 from colorama import Fore, Style, init
 from tomarket import Tomarket, print_timestamp
-import asyncio
 import sys
+import asyncio
 
 
 async def main():
     init(autoreset=True)
 
     tom = Tomarket()
-    tokens = tom.user_login()
+    tokens = await tom.user_login()
 
     for (token, username) in tokens:
         print_timestamp(f"{Fore.CYAN + Style.BRIGHT}[ {username} ]{Style.RESET_ALL}")
-        tom.claim_daily(token=token)
-        tom.user_balance(token=token)
-        tom.start_farm(token=token)
-        tom.list_tasks(token=token)
+        await tom.claim_daily(token=token)
+        await tom.user_balance(token=token)
+        await tom.start_farm(token=token)
+        await tom.list_tasks(token=token)
 
     print_timestamp(f"{Fore.CYAN + Style.BRIGHT}[ Restarting Soon ]{Style.RESET_ALL}")
     await asyncio.sleep(2 * 3600)
