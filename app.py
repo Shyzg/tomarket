1fba84c1bd18743d6b55e97e87078de533bffd93
Al Jarreau Arman Maulana <shyzago@gmail.com>
2024-10-01 15:31:57 +0700

    Aw

--- app.py
+++ app.py
@@ -620,11 +620,7 @@ class Tomarket:
                                 sleep(random.randint(3, 5))
                             else:
                                 farming_times.append(datetime.fromtimestamp(balance['data']['farming']['end_at']).astimezone().timestamp())
-                                self.print_timestamp(
-                                    f"{Fore.CYAN + Style.BRIGHT}[ {account['first_name']} ]{Style.RESET_ALL}"
-                                    f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}"
-                                    f"{Fore.YELLOW + Style.BRIGHT}[ Farm Can Be Claim At {datetime.fromtimestamp(balance['data']['farming']['end_at']).astimezone().strftime('%X %Z')} ]{Style.RESET_ALL}"
-                                )
+                                self.print_timestamp(f"{Fore.YELLOW + Style.BRIGHT}[ Farm Can Be Claim At {datetime.fromtimestamp(balance['data']['farming']['end_at']).astimezone().strftime('%X %Z')} ]{Style.RESET_ALL}")
                         else:
                             self.start_farm(token=account['token'])
                             sleep(random.randint(3, 5))
